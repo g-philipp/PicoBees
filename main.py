11e5fa8bec77ff7d4f7c6f9c123d76a728f0fd08
Philipp Gretscher <philipp.gretscher@oneneo.de>
2023-09-28 14:21:44 +0200

    picoUPS

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import machine
 import src.BME280.bme280_float as bme280
+import src.PicoUPSA.picoUPSa as picoUps
 from src.hx711endail.src.hx711 import *
 
-i2c = machine.I2C(0, sda=machine.Pin(8), scl=machine.Pin(9))
-bme = bme280.BME280(i2c=i2c)
+# BME280
+# bmeI2c = machine.I2C(0, sda=machine.Pin(8), scl=machine.Pin(9))
+# bme = bme280.BME280(i2c=bmeI2c)
 
-# driver = HX711(d_out=5, pd_sck=4)
-hx = hx711(Pin(4), Pin(5))
-hx.set_power(hx711.power.pwr_up)
+# UPS
+ups = picoUps.INA219(addr=0x43)
 
-while True:
-    # 4. wait for readings to settle
-    hx711.wait_settle(hx711.rate.rate_10)
+# # driver = HX711(d_out=5, pd_sck=4)
+# hx = hx711(Pin(4), Pin(5))
+# hx.set_power(hx711.power.pwr_up)
 
-    # 5. read values
+# while True:
+#     # 4. wait for readings to settle
+#     hx711.wait_settle(hx711.rate.rate_10)
 
-    # wait (block) until a value is read
-    val = hx.get_value()
-    print(val)
+#     # 5. read values
 
-print(bme.values[0])
-print(bme.values[1])
-print(bme.values[2])
+#     # wait (block) until a value is read
+#     val = hx.get_value()
+#     print(val)
+
+
+## BME280 ##
+# print(bme.values[0])
+# print(bme.values[1])
+# print(bme.values[2])
+
+## UPS ##
+bus_voltage = ups.getBusVoltage_V()             # voltage on V- (load side)
+current = ups.getCurrent_mA()                   # current in mA
+P = (bus_voltage -3)/1.2*100
+if(P<0):P=0
+elif(P>100):P=100
+
+print("Voltage:  {:6.3f} V".format(bus_voltage))
+print("Current:  {:6.3f} A".format(current/1000))
+print("Percent:  {:6.1f} %".format(P))
